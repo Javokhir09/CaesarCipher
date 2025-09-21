cc7123f40b10a9dcd3561d919e01f57bb536232f
Javohir <javokhirturdikulov@gmail.com>
2025-09-21 10:57:25 +0500

    fix in cors

--- server/main.py
+++ server/main.py
@@ -7,8 +7,8 @@ app = FastAPI()
 
 app.add_middleware(
      CORSMiddleware,
-     allow_origins=["https://cipherer.netlify.app/"],
-     allow_credentials=True,
+     allow_origins=["*"],
+     allow_credentials=False,
      allow_methods=["*"],
      allow_headers=["*"],
 )
